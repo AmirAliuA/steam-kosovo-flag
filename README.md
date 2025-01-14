6898407895319613e3f1e1ca4c5972e88db6a541
Amir Aliu <amiraliudev@gmail.com>
2025-01-15 00:49:06 +0100

    Fix extension to properly check between Akamai and Cloudflare CDN images
    The extension now checks between Akamai CDN images and Cloudflare CDN images. The extension was broken for some time because I wasn't aware, now it's fixed.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # About
 This is a very simple extension that fixes the missing Kosovo flag on steamcommunity profile.
 
-The extension detects when a Steam profile is being browsed on steamcommunity.com and automatically replaces the missing Kosovo flag with the correct one stored within the extension.
+The extension detects if you’re browsing a Steam profile on steamcommunity.com. If it is able to identify the Kosovo flag (represented by a .gif file), it automatically replaces the missing flag with the correct one stored within the extension.
 
 After waiting patiently for years in hopes that Valve would address this oversight, I took matters into my own hands and developed this solution. Thanks for doing nothing over a simple 10 second flag addition Valve.
 
@@ -9,7 +9,8 @@ After waiting patiently for years in hopes that Valve would address this oversig
 https://amiraliu.vercel.app/blog/steam-browser-extension
 
 # Download Extension
-Firefox Browser: https://addons.mozilla.org/en-US/firefox/addon/kosovo-flag-fixer-for-steam/
+Firefox Browser: https://addons.mozilla.org/en-US/firefox/addon/kosovo-flag-fixer-for-steam/\
+Chrome Browser: I’m not paying $5 to upload MY OWN work to a conglomerate. Download the source code and install it locally.
 
 # Result after Installing Extension
 <img src="https://raw.githubusercontent.com/AmirAliuA/steam-kosovo-flag/main/result.png">
--- script.js
+++ script.js
@@ -4,15 +4,15 @@ function replaceFlag() {
   if (profileFlag) {
     var src = profileFlag.src;
 
-    if (src.includes("https://community.akamai.steamstatic.com/public/images/countryflags/xk.gif")) {
+    if (src.includes("https://community.akamai.steamstatic.com/public/images/countryflags/xk.gif") || src.includes("https://community.cloudflare.steamstatic.com/public/images/countryflags/xk.gif")) {
       var newSrc = browser.runtime.getURL("images/kosovo-flag-16x11.png");
 
       profileFlag.src = newSrc;
 
-      console.log("[KosovoFlagFixer] The flag on /id/ or /profiles/ page has been successfully changed.");
-      console.log("[KosovoFlagFixer] oldSrc: " + src + " | newSrc: " + newSrc + ".");
+      console.log("[Kosovo Flag Fixer] The flag on /id/ or /profiles/ page has been successfully changed.");
+      console.log("[Kosovo Flag Fixer] oldSrc: " + src + " | newSrc: " + newSrc + ".");
     } else {
-      console.log("[KosovoFlagFixer] No image with class 'profile_flag' found on /id/ or /profiles/ page.");
+      console.log("[Kosovo Flag Fixer] No image with class 'profile_flag' found on /id/ or /profiles/ page.");
     }
   }
 
@@ -20,22 +20,21 @@ function replaceFlag() {
   var groupstatDiv = document.querySelectorAll(".groupstat");
   groupstatDiv.forEach(function(div) {
     var label = div.querySelector(".label");
-
     if (label && label.textContent.includes("Location")) {
       var image = div.querySelector("img");
     
-      if (image && image.src.includes("https://community.akamai.steamstatic.com/public/images/countryflags/xk.gif")) {
+      if (src.includes("https://community.akamai.steamstatic.com/public/images/countryflags/xk.gif") || src.includes("https://community.cloudflare.steamstatic.com/public/images/countryflags/xk.gif")) {
         var newSrc = browser.runtime.getURL("images/kosovo-flag-16x11.png");
 
         image.src = newSrc;
 
-        console.log("[KosovoFlagFixer] The Kosovo flag on the /groups/ page has been successfully changed.");
-        console.log("[KosovoFlagFixer] oldSrc: " + image.src + " | newSrc: " + newSrc + ".");
+        console.log("[Kosovo Flag Fixer] The Kosovo flag on the /groups/ page has been successfully changed.");
+        console.log("[Kosovo Flag Fixer] oldSrc: " + image.src + " | newSrc: " + newSrc + ".");
       }
     }
   });
 
-  console.log("[KosovoFlagFixer] If there are any errors, please report them here: https://github.com/AmirAliuA/steam-kosovo-flag/issues/new");
+  console.log("[Kosovo Flag Fixer] If there are any errors, please report them here: https://github.com/AmirAliuA/steam-kosovo-flag/issues/new");
 }
 
 // Create a MutationObserver to detect changes in the DOM for dynamically loaded content
